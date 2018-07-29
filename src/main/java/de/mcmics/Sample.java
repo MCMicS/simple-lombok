3c39d4200a97a3c0bcd3eb08b7270b517e490691
mcmics <MCMicS@users.noreply.github.com>
2018-07-29 11:04:12 +0200

    reduce dependencies

--- src/main/java/de/mcmics/Sample.java
+++ src/main/java/de/mcmics/Sample.java
@@ -3,23 +3,15 @@ package de.mcmics;/*
  * Created by MCMicS
  */
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Value;
 import lombok.extern.java.Log;
-
-import javax.annotation.Nonnull;
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.util.logging.Level;
 
 @Log
 @Value
 @AllArgsConstructor
 public class Sample {
 
-    @Nonnull
     private final String name;
 
     public Sample() {
